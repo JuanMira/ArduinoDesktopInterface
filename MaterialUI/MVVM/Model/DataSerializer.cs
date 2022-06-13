8ac66f3b6071741b16b0ec6a7161c41f8bc1e6c3
Sebastian Mira <juanmira720@gmail.com>
2022-06-13 11:46:23 -0500

    Serializer of the model, to convert to object or json

---
+++ MaterialUI/MVVM/Model/DataSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialUI.MVVM.Model
+{
+    internal class DataSerializer
+    {
+        public string Data1 { get; set; }
+        public string Data2 { get; set; }
+        public string Data3 { get; set; }
+        public string Data4 { get; set; }
+        public string Data5 { get; set; }
+        public string Data6 { get; set; }
+
+    }
+}
---
+++ MaterialUI/MVVM/Model/DeviceSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialUI.MVVM.Model
+{
+    internal class DeviceSerializer
+    {
+        public string type { get; set; }
+        public bool deviceStatus { get; set; }
+        public DataSerializer? data { get; set; }
+
+        public override string ToString()
+        {
+            return $"type = {type}; DeviceStatus = {deviceStatus}";
+        }
+    }
+}
